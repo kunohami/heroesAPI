37d0a71ff3d87c364cecac46e918b6bf7fe87b73
Silvia Balmaseda <davsil888@gmail.com>
2026-02-05 17:18:31 +0100

    Create AppDbContext.cs
    Creado el dbcontext

---
+++ heroesAPI/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace heroesAPI.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext() : base()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    // OnModelCreating
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5441;Database=heroesapi;SearchPath=heroescodefirst;Username=heroesuser;Password=Abcd1234");
+
+    }
+
+    // public DbSet<Personaje> Personajes { get; set; }
+
+
+}
+
